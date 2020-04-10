5e3050606d429e55bd6c8a24640c50e6344578b2
eyakub <eyakubsorkar@gmail.com>
2020-04-10 21:04:49 +0600

    Updates

---
+++ precache-manifest.2425dfef2fcd20614b7d9cd976f57f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee534fde6c7c16dbbf3e04bf1e31ef59",
+    "url": "/tic-tac-toe-reactjs/index.html"
+  },
+  {
+    "revision": "fb86c0c1e3d5c24bd648",
+    "url": "/tic-tac-toe-reactjs/static/css/main.0a9e2b55.chunk.css"
+  },
+  {
+    "revision": "a1edb47e95d3db503c3c",
+    "url": "/tic-tac-toe-reactjs/static/js/2.e867a104.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-reactjs/static/js/2.e867a104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb86c0c1e3d5c24bd648",
+    "url": "/tic-tac-toe-reactjs/static/js/main.6dfecbd6.chunk.js"
+  },
+  {
+    "revision": "b3fd2302a2a51c8cf43c",
+    "url": "/tic-tac-toe-reactjs/static/js/runtime-main.2dad8112.js"
+  }
+]);
