a3b618d72ccccdb7a02ebb31cbe6f718e2a973de
eyakub <eyakubsorkar@gmail.com>
2020-04-10 19:28:10 +0600

    game board done

---
+++ src/components/game.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Board from './board';
+
+class Game extends React.Component{
+
+  state = {
+    history: [{squares: Array(9).fill(null)}]
+  }
+
+  onClick = i => {
+    console.log(i)
+  }
+  render(){
+    return(
+      <div>
+        <hr/>
+        <Board onClick={this.onClick} squares={this.state.history[0]} />
+      </div>
+    )
+  }
+}
+
+export default Game;
